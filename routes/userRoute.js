bebc2c4c0de36056e3d9aac4d96e7821b9dadd7c
shorry <ksb334ksb@gmail.com>
2022-04-05 21:27:49 +0900

    ADD: 숙소 좋아요 API 구현 (받아오는 정보 미정)

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/signup', userController.validateForm, userController.signUp);
+router.get('/is-like', userController.isLike);
 
 module.exports = router;
--- services/userService.js
+++ services/userService.js
@@ -14,7 +14,7 @@ const signUp = async (email, password, username, phoneNumber) => {
   }
 
   // duplicated email
-  const userEmail = await userDao.getUserEmailbyEmail(email);
+  const userEmail = await userDao.getUserEmailByEmail(email);
   if (userEmail.length) {
     const err = new Error('EXSITING_USER');
     err.statusCode = 409;
@@ -60,4 +60,21 @@ const signUp = async (email, password, username, phoneNumber) => {
   return createUser;
 };
 
-module.exports = { signUp };
+const isLike = () => {
+  const decoded = jwt.decode(token);
+  // 디코디드 한 토큰에서 email 추출
+
+  // user id, dormitory id 전달하기
+  const isLike = await userDao.getLikeByEmailDorm();
+
+  if (isLike.length) {
+    // isLike 존재하면 좋아요 off => data delete
+    await userDao.likeOff();
+  } else {
+    // isLike 존재하지 않으면 좋아요 on => data insert
+    await userDao.likeOn();
+  }
+  return;
+};
+
+module.exports = { signUp, isLike };
