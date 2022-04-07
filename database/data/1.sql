5c1806ee8fb7a15bb1bac7c5190601885078bba1
Yu081197 <81637903+Yu081197@users.noreply.github.com>
2022-04-07 17:24:25 +0900

    add: first

---
+++ database/data/1.sql
@@ -0,0 +1,2 @@
+-- categories 테이블
+INSERT INTO categories (id, name) VALUES (1, '게스트하우스'), (2,'호텔'), (3, '렌탈 하우스'), (4,'펜션');
--- models/detailDao.js
+++ models/detailDao.js
@@ -3,9 +3,14 @@ const prisma = new PrismaClient();
 
 const getDetail = async () => {
   return await prisma.$queryRaw`
-    SELECT
-    
-    `;
+  SELECT
+  ri.id,
+  ri.image_url AS imageUrl
+  FROM rooms r
+  LEFT JOIN rooms_images ri ON r.id = ri.room_id
+  WHERE ri.id < 7
+  ORDER BY r.id;
+  `;
 };
 
 module.exports = { getDetail };
