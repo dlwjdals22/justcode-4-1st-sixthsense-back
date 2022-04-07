16d3f8ccbbbe7fe144935f1110d3b95d40c09379
Yu081197 <81637903+Yu081197@users.noreply.github.com>
2022-04-07 20:00:00 +0900

    add: 쿼리문 구현중

--- models/detailDao.js
+++ models/detailDao.js
@@ -4,12 +4,71 @@ const prisma = new PrismaClient();
 const getDetail = async () => {
   return await prisma.$queryRaw`
   SELECT
-  ri.id,
-  ri.image_url AS imageUrl
-  FROM rooms r
-  LEFT JOIN rooms_images ri ON r.id = ri.room_id
-  WHERE ri.id < 7
-  ORDER BY r.id;
+  d.name AS dormitories_name,
+  d.comment, 
+  d.main_description, 
+  d.sub_description,
+
+  (SELECT
+    JSON_ARRAYAGG(y.image_url)
+    FROM dormitories x
+    LEFT JOIN dormitories_images y ON y.dormitory_id = x.id
+    WHERE x.id = d.id
+    GROUP BY x.id
+    ) AS main_img_url,
+
+  (SELECT
+    JSON_ARRAYAGG(y.name)
+    FROM dormitories x
+    LEFT JOIN recommend_places y ON y.dormitory_id = x.id
+    WHERE x.id = d.id
+    GROUP BY x.id
+    ) AS name,
+
+  (SELECT
+    JSON_ARRAYAGG(y.type)
+    FROM dormitories x
+    LEFT JOIN recommend_places y ON y.dormitory_id = x.id
+    WHERE x.id = d.id
+    GROUP BY x.id
+    ) AS type,
+
+  (SELECT
+    JSON_ARRAYAGG(y.description)
+    FROM dormitories x
+    LEFT JOIN recommend_places y ON y.dormitory_id = x.id
+    WHERE x.id = d.id
+    GROUP BY x.id
+    ) AS description,
+
+  (SELECT 
+    JSON_ARRAYAGG(y.price)
+    FROM dormitories x
+    LEFT JOIN rooms y ON y.dormitory_id = x.id
+    WHERE x.id = d.id
+    GROUP BY x.id
+    ) AS price,
+
+  (SELECT 
+    JSON_ARRAYAGG(y.head_count) 
+    FROM dormitories x
+    LEFT JOIN rooms y ON y.dormitory_id = x.id
+    WHERE x.id = d.id
+    GROUP BY x.id
+    ) AS headCount,
+
+  (SELECT 
+    JSON_ARRAYAGG(y.image_url)
+    FROM dormitories x
+    LEFT JOIN dormitories_images y ON y.dormitory_id = x.id
+    WHERE x.id = d.id
+    GROUP BY x.id
+  ) AS imageUrl
+
+  FROM dormitories d
+
+
+
   `;
 };
 
