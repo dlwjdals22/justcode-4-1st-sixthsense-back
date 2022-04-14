6f277b7b5f8f259f27b2e6a73f1e529745a7879a
shorry <ksb334ksb@gmail.com>
2022-04-14 12:13:50 +0900

    리팩토링(좋아요api 테스트 후  주석처리) 및 리드미 완성

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,14 +1,13 @@
 const express = require('express');
 
 const userController = require('../controllers/userController');
-const { validateToken } = require('../middleware/authorization');
+// const { validateToken } = require('../middleware/authorization');
 
 const router = express.Router();
 
 router.post('/login', userController.logIn);
 router.post('/signup', userController.signUp);
-router.get('/test', validateToken, userController.test);
-router.post('/heart', validateToken, userController.isLike);
-router.get('/heart', validateToken, userController.showLike);
+// router.post('/heart', userController.isLike);
+// router.get('/heart', userController.showLike);
 
 module.exports = router;
